9690ad6c6e910086a92e0427fee3e9d014238cfd
fda020 <fda020@uit.no>
2024-08-30 00:20:30 +0200

    csv file

--- src/classify.py
+++ src/classify.py
@@ -2,6 +2,6 @@ import numpy as np
 import pandas as pd
 
 data = pd.read_csv("../csv/SpotifyFeatures.csv",comment = '#', delimiter=',', header=0)
-#print(data.shape) #232725 songs
+#print(data.shape) #232725 songs and 18 features 
 genre = np.array(data['genre'].shape)
 print(genre) #232725 songs
