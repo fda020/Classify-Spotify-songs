2bee945c6f753e5ba7f3add07488666bc8607f6b
fda020 <fda020@uit.no>
2024-09-01 15:09:23 +0200

    sorted the code

--- src/classify.py
+++ src/classify.py
@@ -3,54 +3,87 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import display
 
-display.dataframe_display()
+class DataFrame():
+    def __init__(self, datafile):
+        self.all_df = pd.read_csv(datafile,comment = '#', delimiter=',', header=0)
 
-all_df = pd.read_csv("../csv/SpotifyFeatures.csv",comment = '#', delimiter=',', header=0)
-print(all_df.shape) #232725 songs and 18 features 
+    def filtering(self, heading, filter, ind):
+        self.filter_df = self.all_df.loc[self.all_df[heading] == filter]
+        self.filter_df.index = [ind] * len(self.filter_df)
+
+    def retrieve_matrix(self):
+        return np.transpose(np.array([self.filter_df['liveness'],self.filter_df['loudness']]))
+    
+    def retrieve_vector(self):
+        return np.array(self.filter_df.index)
+    
+class train_and_test():
+    def __init__(self, matrix, vector):
+        combine = np.c_[vector, matrix]
+        np.random.shuffle(combine)
+        print(combine)
+
+        #make the matrix and the vector by splitting
+        split_matrix = combine[:, 1:]
+        split_vector = combine[:, 0]
+        print(split_matrix.shape)
+        print(split_vector.shape)
+
+        split_ratio = 0.8
+        split_index = int(split_ratio * len(split_matrix))
+
+        #Make a train and test set for the matrix and the vector
+        self.matrix_train = split_matrix[:split_index]
+        self.vector_train = split_vector[:split_index]
+
+        self.matrix_test = split_matrix[split_index:]
+        self.vector_test = split_vector[split_index:]
 
 
-#Filtering the dataset to only give Classical in a dataframe
-Classical_df = all_df.loc[all_df['genre'] == 'Classical']
-Classical_df.index = [0] * len(Classical_df)
-print(Classical_df.shape) #9256 Classical songs
+# class Log_Reg():
+#     def __init__(self):
+#         pass
 
-#Filtering the dataset to only give Pop in a dataframe
-Pop_df = all_df.loc[all_df['genre'] == 'Pop']
-Pop_df.index = [1] * len(Pop_df)
-print(Pop_df.shape) #9386 Pop songs
+#     def sigmoid(x):
+#         return 1/(1 + np.exp(-x))
 
-#Put the dataframe for pop and classical together
-CP_df = pd.concat([Classical_df, Pop_df])
-print(CP_df)
 
-#To make the dataframe smaller with liveness and loudness
-CP_shorten = CP_df[['liveness', 'loudness']]
-print(CP_shorten)
+if __name__== "__main__":
+    display.dataframe_display()
+    dataframe = DataFrame("../csv/SpotifyFeatures.csv")
+    print(dataframe.all_df.shape) #232725 songs and 18 features 
 
-#Matrix where rows are songs and the two columns are liveness and loudness
-songs = np.transpose(np.array([CP_shorten['liveness'],CP_shorten['loudness']]))
-print(songs)
-#The vector as labels
-genre = np.array(CP_shorten.index)
-print(genre)
+    Classical_df = DataFrame("../csv/SpotifyFeatures.csv")
+    Classical_df.filtering('genre', 'Classical', 0) 
+    print(Classical_df.filter_df.shape) #9256 Classical songs
 
-#Combine to shuffle all data before making a test and training set
-combine = np.c_[genre, songs]
-np.random.shuffle(combine)
-print(combine)
+    Pop_df = DataFrame("../csv/SpotifyFeatures.csv")
+    Pop_df.filtering('genre', 'Pop', 1) 
+    print(Pop_df.filter_df.shape) #9386 Pop songs
+    
+    # Put the dataframes together
+    CP_df = DataFrame("../csv/SpotifyFeatures.csv")
+    CP_df.filter_df = pd.concat([Classical_df.filter_df, Pop_df.filter_df])
+    #Make dataframe to only view liveness and loudness 
+    CP_df.filter_df = CP_df.filter_df[['liveness', 'loudness']]
+    print(CP_df.filter_df)
 
-#make the matrix and the vector by overwriting 
-songs = combine[:, 1:]
-genre = combine[:, 0]
-print(songs.shape)
-print(genre.shape)
+    song_matrix = CP_df.retrieve_matrix()
+    print(song_matrix)
 
-#Make a train and test set for the matrix and the vector
-split_ratio = 0.8
-split_index = int(split_ratio * len(songs))
+    genre_vector = CP_df.retrieve_vector()
+    print(genre_vector)
+    
+    train = train_and_test(song_matrix, genre_vector)
+    matrix_train = train.matrix_train
+    vector_train = train.vector_train
 
-songs_train = songs[:split_index]
-genre_train = genre[:split_index]
+    matrix_test = train.matrix_test
+    vector_test = train.vector_test
+    
+    print(matrix_train)
 
-songs_test = songs[split_index:]
-genre_test = genre[split_index:]
+    
+
+    
+    
